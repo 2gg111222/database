ac6cc08d5185bfb5cfc225909dd19ea96ba5d039
2gg111222 <124110995+2gg111222@users.noreply.github.com>
2023-02-23 20:57:10 +0900

    Add files via upload

---
+++ 데이터베이스1/Script-2.sql
@@ -0,0 +1,13 @@
+-- HR.HOBBY definition
+
+CREATE TABLE "HR"."HOBBY2" 
+   (	"HOBBYID" number(38,0),
+    
+	"CONTENT" VARCHAR2(100), 
+	"LOCATION" VARCHAR2(100)
+   ) 
+   
+   INSERT INTO "HOBBY2" VALUES ('100' , 'run', 'seoul')
+   INSERT INTO "HOBBY2" VALUES ('200' , 'book', 'busan')
+   
+   
